6d56865d1b597d98d6d5f22f7380d4b7fcfb0bba
williamhhk <william.han@gmail.com>
2017-01-07 08:38:34 -0700

    Check in

--- ReactJSDemo/Scripts/app/Index.jsx
+++ ReactJSDemo/Scripts/app/Index.jsx
@@ -1,52 +1,4 @@
-﻿const status = {
-    a: 'abc',
-    b: 'bcde'
-};
-
-class NameForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { formName : ''}
-
-        this.handleFormName = this.handleFormName.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleFormName(event) {
-        console.log(event.target.value);
-        this.setState({ formName: event.target.value });
-    }
-
-    handleSubmit(event) {
-        console.log(this.state.formName);
-        axios.post(this.props.UrlPost, { FirstName: this.state.formName })
-          .then(function (response) {
-              //console.log('saved successfully')
-              //console.log(response.data.status);
-              //console.log(response.data.message);
-          });
-
-        event.preventDefault();
-    }
-
-    render() {
-        const FormName = this.state.formName;
-        console.log(this.state.formName);
-        console.log(this.props.Name);
-        return (
-          <form onSubmit={this.handleSubmit}>
-        <label>
-            Name:
-          <input type="text" value={this.props.Name}  />
-          <input type="text" value={FormName}  onChange={this.handleFormName}  />
-        </label>
-         <input type="submit" value="Submit" />
-      </form>
-    );
-        }
-}
-
-class ComponentWithGriddle extends React.Component {
+﻿class ComponentWithGriddle extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -54,42 +6,80 @@ class ComponentWithGriddle extends React.Component {
         };
     }
     onRowClick(row) {
+        console.log(row.props.data);
         this.setState({ selectedRowId: row.props.data.id });
-        this.props.onChange(row.props.data.name);
+        this.props.onUserInput(row.props.data.name);
     }
     render() {
         var rowMetadata = {bodyCssClassName: rowData => (rowData.id === this.state.selectedRowId ? "selected" : '')};
-        //console.log(rowMetadata);
         return (
         <Griddle results={fakeData} onRowClick={this.onRowClick.bind(this)} showFilter={true} showSettings={true} columnMetadata={columnMeta} useFixedHeader={true} rowMetadata={rowMetadata} resultsPerPage={5} enableInfiniteScroll={true} bodyHeight={400 } useGriddleStyles={true} />
         );
      }
 }
 
-
 class RootFrame extends React.Component {
     constructor(props) {
         super(props);
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.state = { Name: '' };
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.state = { inputName: '' };
     }
 
-    handleNameChange(name)
+    handleUserInput(name)
     {
         console.log("Name is  " + name);
-        this.setState({ Name: name });
+        this.setState({ inputName: name });
     }
 
-    //             <NameForm UrlPost={this.props.UrlPost}  Name={this.state.Name}/>
-    //  Use Parent as a state manager, once this is set, Child Component cannot change.
     render() {
         return (
         <div>
-            <ComponentWithGriddle Name={this.state.Name} onChange={this.handleNameChange} />
-            <NameForm UrlPost={this.props.UrlPost}  Name={this.state.Name} />
+          <ComponentWithGriddle inputName={this.state.inputName} onUserInput={this.handleUserInput} />
+          <TextInput UrlPost={this.props.UrlPost} txtInputField={this.state.inputName}  onUserInput={this.handleUserInput}/>
         </div>
        );
     };
+}
+
+class TextInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        console.log(e.target.value);
+        this.props.onUserInput(
+            this.txtInputValue.value
+        );
+    }
+    
+    handleSubmit(event) {
+        console.log(this.txtInputValue.value);
+        axios.post(this.props.UrlPost, { firstName: this.txtInputField.value })
+          .then(function (response) {
+          });
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+                Name:
+                <input 
+                       type="text" 
+                       placeholder="Enter/Select..."
+                       value={this.props.txtInputField} 
+                       ref={(input) => this.txtInputValue = input}
+                       onChange={this.handleChange}
+                 />
+            </label>
+                <input type="submit" value="Submit" />
+          </form>
+    );
+        }
 }
 
 ReactDOM.render(
